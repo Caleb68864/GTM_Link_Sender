70ead97c00ae68b0510e9c7eff25ff0853cf76d6
Caleb Bennett <serenityvalley06@gmail.com>
2018-04-03 07:34:38 -0500

    Updated

--- RunAsAdmin.bat
+++ RunAsAdmin.bat
@@ -1,3 +1,3 @@
-rem Runs the script as Admin to enable privileges to kill sessions.
+rem Runs the script as Admin to enable privileges to copy files across network.
 rem Change the Admin Account, python location and script location to fit your environment.
 runas /user:capc\<Admin Account> "<Path to Python> <Path to __main__.py>"
